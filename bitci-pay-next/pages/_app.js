f3f992fd13a5f633d135ba6bea02f86f7ada9c18
emre15900 <79264023+emre15900@users.noreply.github.com>
2021-08-06 15:07:02 +0300

    v2.4

--- bitci-pay-next/pages/_app.js
+++ bitci-pay-next/pages/_app.js
@@ -1,12 +1,11 @@
-import '../styles/globals.scss'
-import React from 'react';
-import App from 'next/app';
+import "../styles/globals.scss";
+import React from "react";
+import App from "next/app";
 
-import 'bootstrap/scss/bootstrap.scss';
-
+import "bootstrap/scss/bootstrap.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- bitci-pay-next/pages/index.js
+++ bitci-pay-next/pages/index.js
@@ -93,7 +93,6 @@ export default function Home() {
           </div>
         </section>
       </main>
-
     </Layout>
   );
 }
